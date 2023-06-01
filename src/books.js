23aaffb40adc6c57dc693cc008781ea8a39c5a34
zulvaaraf <zulvasebelas@gmail.com>
2023-06-01 18:41:03 +0700

    objek constructor

--- src/books.js
+++ src/books.js
@@ -17,6 +17,27 @@ class Books {
     this.insertedAt = currentTime;
     this.updatedAt = currentTime;
   }
+
+  updateBook(newData) {
+    this.name = newData.name;
+    this.year = newData.year;
+    this.author = newData.author;
+    this.summmary = newData.summary;
+    this.publisher = newData.publisher;
+    this.pageCount = newData.pageCount;
+    this.readPage = newData.readPage;
+    this.finished = newData.finished;
+    this.reading = newData.reading;
+    this.updatedAt = new Date().toISOString();
+  }
+
+  getIdNameAndPublisher() {
+    return {
+      id: this.id,
+      name: this.name,
+      publisher: this.publisher,
+    };
+  }
 }
 
 module.exports = Books;
